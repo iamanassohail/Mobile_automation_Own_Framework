0e46f5ec295da5e762980f3de662ce78114905d1
Anas Sohail <anas.sohail@venturedive.com>
2024-06-22 15:47:09 +0500

    Added comments

--- src/test/java/pageobject/Scn1.java
+++ src/test/java/pageobject/Scn1.java
@@ -3,37 +3,28 @@ package pageobject;
 import general.BaseTest;
 import general.CommonFunctions;
 import io.appium.java_client.MobileBy;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.ElementOption;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.Test;;
-import general.CommonFunctions.*;
-
-import java.time.Duration;
 import java.util.List;
 
 public class Scn1 extends BaseTest {
 
     public static void scenario1Test() throws InterruptedException {
-        // Navigate to the required section (assuming it's a button click)
+        // Navigating to the required section (assuming it's a button click)
         WebElement wrapOption = (WebElement) driver.findElement(By.id("65e5cdd0-2c27-43fc-93b7-7034fd37ba52")); // Replace with actual ID
         wrapOption.click();
 
-        // Select Nigeria by scrolling
+        // Selecting Nigeria by scrolling
         CommonFunctions.scrollToElement("Nigeria");
 
-        // Drag Nigeria to the top
+        // Dragging Nigeria to the top
         WebElement nigeriaElement = (WebElement) driver.findElement(MobileBy.AndroidUIAutomator(
                 "new UiSelector().text(\"Nigeria\")"
         ));
         CommonFunctions.dragAndDrop(nigeriaElement, 0);
 
-        // Remove Afghanistan by swiping left
+        // Removing Afghanistan by swiping left
         WebElement afghanistanElement = (WebElement) driver.findElement(MobileBy.AndroidUIAutomator(
                 "new UiSelector().text(\"Afghanistan\")"
         ));
--- src/test/java/pageobject/Scn2.java
+++ src/test/java/pageobject/Scn2.java
@@ -1,31 +1,24 @@
 package pageobject;
 
 import general.BaseTest;
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.ElementOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 
 public class Scn2 extends BaseTest {
 
     public static void scenario2Test() throws InterruptedException {
-        // Navigate back to home screen
+        // Navigating back to home screen
         driver.navigate().back();
 
-        // Select the "Multi choice questions" section
+        // Selecting the "Multi choice questions" section
         WebElement multiChoiceSection = driver.findElement(By.id("27b63f52-17d9-4c24-a964-cd5c52f8b66f")); // Replace with actual ID
         multiChoiceSection.click();
 
-        // Select 5 radio buttons randomly
+        // Selecting 5 radio buttons randomly
         List<WebElement> radioButtons = driver.findElements(By.className("android.widget.CheckedTextView"));
         Random random = new Random();
         for (int i = 0; i < 5; i++) {
@@ -35,7 +28,7 @@ public class Scn2 extends BaseTest {
             }
         }
 
-        // Delete all selected items
+        // Deleting all selected items
         for (WebElement radioButton : radioButtons) {
             if (radioButton.isSelected()) {
                 WebElement deleteButton = radioButton.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.view.ViewGroup[1]/android.widget.LinearLayout/android.widget.ImageView[2]")); // Adjust XPath if necessary
@@ -43,24 +36,24 @@ public class Scn2 extends BaseTest {
             }
         }
 
-        // Kill the activity
+        // Killing the activity
         ((AndroidDriver) driver).terminateApp("com.mobeta.android.demodslv"); // Replace with actual app package name
 
-        // Start the 2nd activity (2nd application)
+        // Starting the 2nd application
         ((AndroidDriver) driver).activateApp("io.selendroid.testapp"); // Replace with actual second app package name
 
         // Wait dynamically until the 2nd application launches
         Thread.sleep(5000); // Adjust the waiting time if needed
 
         // Assertions
-        // Check that deleted items are not in the list
+        // Checking that deleted items are not in the list
         multiChoiceSection.click(); // Navigate back to the multi-choice section
         radioButtons = driver.findElements(By.className("android.widget.CheckedTextView"));
         for (WebElement radioButton : radioButtons) {
             Assert.assertFalse(radioButton.isSelected(), "Deleted items should not be present in the list");
         }
 
-        // Check that the 2nd application is open
+        // Checking that the 2nd application is open
         boolean isSecondAppOpened = ((AndroidDriver) driver).isAppInstalled("io.selendroid.testapp"); // Replace with actual second app package name
         Assert.assertTrue(isSecondAppOpened, "The 2nd application should be opened successfully");
     }
