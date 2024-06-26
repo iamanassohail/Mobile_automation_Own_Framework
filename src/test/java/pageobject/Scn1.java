78c103bf854100c40aa61a2823a05f28a8b350bb
Anas Sohail <anas.sohail@venturedive.com>
2024-06-26 22:52:58 +0500

    irrelevant messages removed

--- src/test/java/pageobject/Scn1.java
+++ src/test/java/pageobject/Scn1.java
@@ -11,27 +11,22 @@ import java.util.List;
 public class Scn1 extends BaseTest {
 
     public static void scenario1Test() throws InterruptedException {
-        // Navigating to the required section (assuming it's a button click)
-        WebElement wrapOption = (WebElement) driver.findElement(By.id("65e5cdd0-2c27-43fc-93b7-7034fd37ba52")); // Replace with actual ID
+        WebElement wrapOption = (WebElement) driver.findElement(By.id("65e5cdd0-2c27-43fc-93b7-7034fd37ba52"));
         wrapOption.click();
 
-        // Selecting Nigeria by scrolling
         CommonFunctions.scrollToElement("Nigeria");
 
-        // Dragging Nigeria to the top
         WebElement nigeriaElement = (WebElement) driver.findElement(MobileBy.AndroidUIAutomator(
                 "new UiSelector().text(\"Nigeria\")"
         ));
         CommonFunctions.dragAndDrop(nigeriaElement, 0);
 
-        // Removing Afghanistan by swiping left
         WebElement afghanistanElement = (WebElement) driver.findElement(MobileBy.AndroidUIAutomator(
                 "new UiSelector().text(\"Afghanistan\")"
         ));
         CommonFunctions.swipeLeft(afghanistanElement);
 
-        // Assertions
-        List<WebElement> items = driver.findElements(By.id("b7fc9ba7-6ff6-4168-8ba9-01b40f818de4")); // Replace with actual item ID
+        List<WebElement> items = driver.findElements(By.id("b7fc9ba7-6ff6-4168-8ba9-01b40f818de4"));
         Assert.assertEquals(items.get(0).getText(), "Nigeria");
         boolean isAfghanistanPresent = items.stream().anyMatch(item -> item.getText().equals("Afghanistan"));
         Assert.assertFalse(isAfghanistanPresent);
--- src/test/java/pageobject/Scn2.java
+++ src/test/java/pageobject/Scn2.java
@@ -11,14 +11,11 @@ import java.util.Random;
 public class Scn2 extends BaseTest {
 
     public static void scenario2Test() throws InterruptedException {
-        // Navigating back to home screen
         driver.navigate().back();
 
-        // Selecting the "Multi choice questions" section
         WebElement multiChoiceSection = driver.findElement(By.id("27b63f52-17d9-4c24-a964-cd5c52f8b66f")); // Replace with actual ID
         multiChoiceSection.click();
 
-        // Selecting 5 radio buttons randomly
         List<WebElement> radioButtons = driver.findElements(By.className("android.widget.CheckedTextView"));
         Random random = new Random();
         for (int i = 0; i < 5; i++) {
@@ -28,7 +25,6 @@ public class Scn2 extends BaseTest {
             }
         }
 
-        // Deleting all selected items
         for (WebElement radioButton : radioButtons) {
             if (radioButton.isSelected()) {
                 WebElement deleteButton = radioButton.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.view.ViewGroup[1]/android.widget.LinearLayout/android.widget.ImageView[2]")); // Adjust XPath if necessary
@@ -36,25 +32,19 @@ public class Scn2 extends BaseTest {
             }
         }
 
-        // Killing the activity
-        ((AndroidDriver) driver).terminateApp("com.mobeta.android.demodslv"); // Replace with actual app package name
+        ((AndroidDriver) driver).terminateApp("com.mobeta.android.demodslv");
 
-        // Starting the 2nd application
-        ((AndroidDriver) driver).activateApp("io.selendroid.testapp"); // Replace with actual second app package name
+        ((AndroidDriver) driver).activateApp("io.selendroid.testapp");
 
-        // Wait dynamically until the 2nd application launches
-        Thread.sleep(5000); // Adjust the waiting time if needed
+        Thread.sleep(5000);
 
-        // Assertions
-        // Checking that deleted items are not in the list
-        multiChoiceSection.click(); // Navigate back to the multi-choice section
+        multiChoiceSection.click();
         radioButtons = driver.findElements(By.className("android.widget.CheckedTextView"));
         for (WebElement radioButton : radioButtons) {
             Assert.assertFalse(radioButton.isSelected(), "Deleted items should not be present in the list");
         }
 
-        // Checking that the 2nd application is open
-        boolean isSecondAppOpened = ((AndroidDriver) driver).isAppInstalled("io.selendroid.testapp"); // Replace with actual second app package name
+        boolean isSecondAppOpened = ((AndroidDriver) driver).isAppInstalled("io.selendroid.testapp");
         Assert.assertTrue(isSecondAppOpened, "The 2nd application should be opened successfully");
     }
 }
